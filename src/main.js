fef0c1b8b6e34cd430532c151f6cdde2e9c56ffe
Coder01 <mnkcoders@gmail.com>
2025-02-15 20:35:21 +0100

    prepared course project's module

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import {Solar} from './solar.js';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+//import './course.js';
+
 //scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(30,window.innerWidth/window.innerHeight,0.01,100000);
@@ -19,19 +21,6 @@ controls.minDistance = 30;
 
 //setup solar system
 const solarSystem = Solar.CreateSystem(scene);
-/*const solarSystem = new Solar();
-scene.add(solarSystem.createStar('Sun','yellow',6).mesh());
-scene.add(solarSystem.createStar('Mercury','orange',0.4,9,0.76,solarSystem.star('Sun')).mesh());
-scene.add(solarSystem.createStar('Venus','#ddaa55',0.95,16,0.54,solarSystem.star('Sun')).mesh());
-scene.add(solarSystem.createStar('Earth','blue',1,24,0.23,solarSystem.star('Sun')).mesh());
-scene.add(solarSystem.createStar('Moon','grey',0.4,2,0.6,solarSystem.star('Earth')).mesh());
-scene.add(solarSystem.createStar('Mars','red',0.8,32,0.16,solarSystem.star('Sun')).mesh());
-scene.add(solarSystem.createStar('Jupiter','maroon',2.4,39,0.1,solarSystem.star('Sun')).mesh());
-scene.add(solarSystem.createStar('Saturn','#5588cc',1.8,45,0.08,solarSystem.star('Sun')).mesh());
-scene.add(solarSystem.createStar('Uranus','#ddeeff',2.1,52,0.05,solarSystem.star('Sun')).mesh());
-scene.add(solarSystem.createStar('Neptune','#aabbff',2,58,0.04,solarSystem.star('Sun')).mesh());
-scene.add(solarSystem.createStar('Pluto','#0055dd',0.5,69,0.024,solarSystem.star('Sun')).mesh());
-*/
 
 //resize
 window.addEventListener('resize',() => {
@@ -40,12 +29,8 @@ window.addEventListener('resize',() => {
   renderer.setSize(window.innerWidth,window.innerHeight);
 });
 
-//initialize randomly the planets
-//solarSystem.initialize();
-
-const clock = new THREE.Clock(false);
-console.log(solarSystem);
-
+//initialize the clock
+const clock = new THREE.Clock();
 //update and drwa
 const draw = () =>{
 
@@ -56,5 +41,4 @@ const draw = () =>{
   renderer.render(scene,camera);
   window.requestAnimationFrame(draw);
 };
-clock.start();
 draw();
